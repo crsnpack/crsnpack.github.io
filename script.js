173007fed59b5be945fd81c0dc4f494958989dc4
beastpuncher <beastpuncher@gmail.com>
2019-02-02 23:26:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -11,5 +11,5 @@ xhr.addEventListener("readystatechange", function () {
 
 xhr.open("GET", "https://www.bungie.net/platform/GroupV2/2152633/Members/");
 xhr.setRequestHeader("x-api-key", "c78d73dcfe79494ca4152a4cfdb86411");
-
 xhr.send(data);
+console.log(JSON.parse(xhr.responseText));
