837a6db11b81182bdba093115345064d695c1a5d
beastpuncher <beastpuncher@gmail.com>
2019-02-02 23:25:18 -0700

    api pt 1

---
+++ script.js
@@ -0,0 +1,15 @@
+var data = null;
+
+var xhr = new XMLHttpRequest();
+xhr.withCredentials = true;
+
+xhr.addEventListener("readystatechange", function () {
+  if (this.readyState === 4) {
+    console.log(this.responseText);
+  }
+});
+
+xhr.open("GET", "https://www.bungie.net/platform/GroupV2/2152633/Members/");
+xhr.setRequestHeader("x-api-key", "c78d73dcfe79494ca4152a4cfdb86411");
+
+xhr.send(data);
